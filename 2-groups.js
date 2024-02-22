cc755b74f428b6233c30ff0d91d3431f9b6e2c34
Person098123 <143832410+Person098123@users.noreply.github.com>
2024-02-22 10:42:19 -0800

    Finished the first two functions and am now on the 3rd function (iterable)

--- 2-groups.js
+++ 2-groups.js
@@ -1,5 +1,6 @@
 /*
-Write a class called Group (since Set is already taken). Like Set, it has add, delete, and has methods. Its constructor creates an empty group, add adds a value to the group (but only if it isn’t already a member), delete removes its argument from the group (if it was a member), and has returns a Boolean value indicating whether its argument is a member of the group.
+Write a class called Group (since Set is already taken). Like Set, it has add, delete, and has methods. Its constructor creates an empty group, add adds a value to the group 
+(but only if it isn’t already a member), delete removes its argument from the group (if it was a member), and has returns a Boolean value indicating whether its argument is a member of the group.
 
 Use the === operator, or something equivalent such as indexOf, to determine whether two values are the same.
 
@@ -7,7 +8,27 @@ Give the class a static from method that takes an iterable object as argument an
 */
 
 class Group {
-    // Your code here.
+    // Your code here
+    constructor(){
+      this.members=[];
+    }
+    add(val){
+      if(!this.has(val))
+      this.members.push(val);
+    }
+    delete(val){
+      this.members=this.members.filter(v=>v !==val);
+    }
+    has(val){
+      return this.members.includes(val);
+    }
+    static from(collection){
+      let group = new Group
+      for(let val of collection){
+      group.add(val);
+      }
+      return group;
+    }
   }
   
 
