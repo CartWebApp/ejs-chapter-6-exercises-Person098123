01d37ba1778704686bbbed20fcd6e7a087db1014
Person098123 <143832410+Person098123@users.noreply.github.com>
2024-02-23 10:43:52 -0800

    finished all of them

--- 3-iterable.js
+++ 3-iterable.js
@@ -7,8 +7,49 @@ It is okay if your iterator behaves strangely when the group is modified during 
 */
 
 // Your code here (and the code from the previous exercise)
+class Group {
+  // Your code here
+  constructor(){
+    this.members=[];
+  }
+  add(val){
+    if(!this.has(val))
+    this.members.push(val);
+  }
+  delete(val){
+    this.members=this.members.filter(v=>v !==val);
+  }
+  has(val){
+    return this.members.includes(val);
+  }
+  static from(collection){
+    let group = new Group
+    for(let val of collection){
+    group.add(val);
+    }
+    return group;
+  }
 
+[Symbol.iterator](){
+  return new GroupIterator(this);
+}
+} 
 
+class GroupIterator{
+constructor(group){
+  this.group=group
+  this.position=0
+}
+next(){
+  if(this.position>=this.group.members.length){
+    return {done: true};
+  } else{
+    let result = {value: this.group.members[this.position], done: false};
+    this.position++;
+    return result;
+  }
+}
+}
 // Tests:
 for (let value of Group.from(["a", "b", "c"])) {
     console.log(value);
